9c9e940ed6dd328f153007cc23f34c7e89833328
itchy <richard.h.styron@gmail.com>
2013-10-30 22:09:50 -0400

    have working parallel w/ dataframe writing issue worked out.

---
+++ scripts/eq_likelihood_mc_joblib.py
@@ -0,0 +1,71 @@
+import sys
+sys.path.append('../eq_stats')
+
+import numpy as np
+import pandas as pd
+import eq_stats as eqs
+import time
+from joblib import Parallel, delayed
+
+# problem setup
+
+t0 = time.time()
+
+# read in fault data table
+f = pd.read_csv('../data/lanf_stats.csv', index_col=0) 
+
+# define some constants and parameters
+n_eq_samp = 2.5e4 # number of earthquakes in time series
+time_window = np.hstack( (1, np.arange(5, 105, step=5) ) ) # observation times
+mc_iters = 2e3
+mc_index = np.arange(mc_iters, dtype='int')
+mc_cols = ['dip', 'Ddot'] + [t for t in time_window]
+min_mag = 6.0 # minimum magnitude for probability searches
+print min_mag
+
+# define frequency-magnitude distribution
+M_vec = np.linspace(5, 7.64, num=1000)
+FM_vec = eqs.F(M_vec)
+
+
+# First run on South Lunggar Detachment
+slr = f.loc['s_lunggar'] # select S. Lunggar fault data from data table
+
+#DF for SLR MC, >= M5 eqs
+slr_6 = pd.DataFrame(index=mc_index, columns=mc_cols, dtype='float')
+
+slr_6.dip, slr_dip_frac = eqs.dip_rand_samp( slr['dip_deg'], 
+                                             slr['dip_err_deg'], mc_iters)
+
+slr_6.Ddot = eqs.Ddot_rand_samp( slr['slip_rate_mm_a'], 
+                                      slr['sr_err_mm_a'], mc_iters)
+
+# define function in script so as not to have to pass all the variables
+# or abstract things too much
+def index_loop(df, index):
+    ss = df.iloc[index]
+    M_samp = eqs.sample_from_pdf(M_vec, FM_vec, n_eq_samp)
+    Mo_samp = eqs.calc_Mo_from_M(M_samp)
+
+    recur_int = eqs.calc_recurrence_interval(Mo=Mo_samp, dip=ss['dip'],
+                                             slip_rate=ss['Ddot'],
+                                             L=slr['L_km'], z=slr['z_km'])
+
+    cum_yrs = eqs.calc_cumulative_yrs(recur_int)
+
+    eq_series = eqs.make_eq_time_series(M_samp, cum_yrs)  # maybe store in dict
+    
+    for t in time_window:
+        ss[t] = ( eqs.get_prob_above_val_in_window(eq_series, min_mag, t) 
+                * slr_dip_frac )
+    return ss
+
+# 
+series_list = Parallel(n_jobs=-2) (delayed( index_loop)(slr_6, ii) 
+                                    for ii in mc_index)
+
+for ii, ser in enumerate(series_list):
+    ind = ser.index
+    slr_6.iloc[ii][list(ind)] = ser.values
+
+
