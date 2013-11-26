7e58e5b62cdc8d7f3b013b4cf16562e7258516a5
richard styron <richard.h.styron@gmail.com>
2013-11-26 14:33:47 -0500

    committing before running full characteristic calcs on burgers

--- scripts/calc_characteristic_eq_probs.py
+++ scripts/calc_characteristic_eq_probs.py
@@ -14,24 +14,22 @@ f = pd.read_csv('../data/lanf_stats.csv', index_col=0)
 
 # define some constants and parameters
 n_cores = -4 # number of cores for parallel processing
-n_eq_samp = 3e4 # number of earthquakes in time series
+n_eq_samp = 5e4
 time_window = np.hstack( (1, np.arange(5, 105, step=5) ) ) # observation times
 mc_iters = 2e3 # number of Monte Carlo iterations
 mc_index = np.arange(mc_iters, dtype='int')
 mc_cols = ['dip', 'Ddot'] + [t for t in time_window]
 max_eq_slip = 15 #m
+char_eq_slip= 1.5 #m
+Mc = 7.64 # Hypothetical corner magnitude for Continental Rift Boundaries
 
-# define frequency-magnitude distribution  not needed in this instance
-Mc = 7.64
-#M_vec = np.linspace(5, Mc, num=1000)
-#FM_vec = eqs.F(M=M_vec, Mc=Mc)
-
-# load fault data and make dfs for each minimum search magnitude
+# load fault data and make dataframes for each minimum search magnitude
 min_M_list = [5, 5.5, 6, 6.5, 7, 7.5]
 
 df_ind_tuples = [[i, M] for i in mc_index for M in min_M_list]
 df_multi_ind = pd.MultiIndex.from_tuples(df_ind_tuples, names=['mc_iter','M'])
 
+rec_int_bins = np.logspace(1, 5)  # bins for recurrence interval statistics
 
 # define function to calculate probabilities for each iteration
 # function is defined here so it can access all variables
@@ -40,9 +38,9 @@ def calc_iter_probs(iter):
     df_iter['dip'] = mc_d['dip_samp'][iter]
     df_iter['Ddot'] = mc_d['Ddot_samp'][iter]
 
-    # Generate EQ sample/sequence from F_char(M) dist.
+    # Generate EQ sample/sequence from F(M) dist.
     m_vec = np.linspace(5, mc_d['max_M'][iter], num=1000)
-    fm_vec = eqs.F_char(m_vec, Mc=Mc, char_M=6.25, char_amplitude_scale=5.25)
+    fm_vec = eqs.F_char(m_vec, Mc=Mc, char_M=mc_d['char_M'][iter])
     M_samp = eqs.sample_from_pdf(m_vec, fm_vec, n_eq_samp)
     Mo_samp = eqs.calc_Mo_from_M(M_samp)
     
@@ -60,11 +58,20 @@ def calc_iter_probs(iter):
     for t in time_window:
         roll_max = pd.rolling_max(eq_series, t)
         df_iter[t] = (eqs.get_probability_above_value(roll_max, min_M_list)
-                      * mc_d['dip_frac'] )
+                      * mc_d['dip_frac'])
+        
+    # calculate histgrams of recurrence intervals
+    rec_int_counts_df = rec_int_df.loc[iter].copy()
+    for mm in min_M_list:
+        ints = np.diff( np.where(eq_series >= mm) )
+        rec_int_counts_df.loc[mm] = np.histogram(ints, bins=rec_int_bins)[0]
+        
 
-    return df_iter
+    return df_iter, rec_int_counts_df
 
-# run for all faults 
+t_init = time.time()
+
+# do calculations, in parallel
 for fault in list(f.index):
     fdf = pd.DataFrame(index=df_multi_ind, columns=mc_cols, dtype='float')
     params = f.loc[fault]
@@ -82,11 +89,25 @@ for fault in list(f.index):
                                                    D=max_eq_slip)
 
     mc_d['max_M'] = eqs.calc_M_from_Mo(mc_d['max_Mo'])
-
+    
+    mc_d['char_Mo'] = eqs.calc_Mo_from_fault_params(L=params['L_km'], 
+                                                    z=params['z_km'], 
+                                                    dip=mc_d['dip_samp'], 
+                                                    D=char_eq_slip)
+    
+    mc_d['char_M'] = eqs.calc_M_from_Mo(mc_d['char_Mo'])
+    
+    rec_int_df = pd.DataFrame(columns = rec_int_bins[1:],
+                              index=df_multi_ind, dtype='float')
     t0 = time.time()
-    prob_list = Parallel(n_jobs=n_cores)( delayed( calc_iter_probs)(ii) 
+    out_list = Parallel(n_jobs=n_cores)( delayed( calc_iter_probs)(ii) 
                                     for ii in mc_index)
     print 'done with', fault, 'parallel calcs in {} s'.format((time.time()-t0))
     for ii in mc_index:
-        fdf.loc[ii][:] = prob_list[ii]
-    fdf.to_csv('../results/{}_F_char.csv'.format(fault))    
+        fdf.loc[ii][:] = out_list[ii][0]
+        rec_int_df.loc[ii][:] = out_list[ii][1]
+        
+    fdf.to_csv('../results/{}_char_test.csv'.format(fault))
+    rec_int_df.to_csv('../results/{}_char_rec_ints_test.csv'.format(fault))
+
+print 'done with all faults in {} s'.format((time.time()-t_init))
